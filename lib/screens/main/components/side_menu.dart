749aa16376fa3bb6e0a3c30b673f75d7501d11bf
shahbajjamil <shahbajjamil2014@gmail.com>
2021-10-17 12:16:55 +0530

    Side menu completed

--- lib/screens/main/components/side_menu.dart
+++ lib/screens/main/components/side_menu.dart
@@ -1,10 +1,12 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutterportfolio/components/animated_progress_indicator.dart';
 import 'package:flutterportfolio/constants.dart';
 import 'package:flutterportfolio/screens/main/components/area_info_text.dart';
 import 'package:flutterportfolio/screens/main/components/coding.dart';
+import 'package:flutterportfolio/screens/main/components/knowledges.dart';
 import 'package:flutterportfolio/screens/main/components/my_info.dart';
 import 'package:flutterportfolio/screens/main/components/skills.dart';
 
@@ -39,6 +41,51 @@ class SideMenu extends StatelessWidget {
                 Skills(),
                 SizedBox(height: defaultPadding),
                 Coding(),
+                Knowledge(),
+                Divider(),
+                SizedBox(width: defaultPadding / 2),
+                TextButton(
+                  onPressed: () {},
+                  child: FittedBox(
+                    child: Row(
+                      children: [
+                        Text(
+                          "DOWNLOAD CV",
+                          style: TextStyle(
+                              color:
+                                  Theme.of(context).textTheme.bodyText1!.color),
+                        ),
+                        SizedBox(width: defaultPadding / 2),
+                        SvgPicture.asset("assets/icons/download.svg"),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: defaultPadding / 2),
+                  color: Color(0xff24242e),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: SvgPicture.asset("assets/icons/linkedin.svg"),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: SvgPicture.asset("assets/icons/github.svg"),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: SvgPicture.asset("assets/icons/twitter.svg"),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: SvgPicture.asset("assets/icons/dribble.svg"),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ))
